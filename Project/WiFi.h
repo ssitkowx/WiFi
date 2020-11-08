331296424784b6fd84185a53c50ba804c5cf3057
sylsit <sitkowskis@interia.pl>
2020-11-08 21:56:47 +0100

    Refactor to CRTP.

--- Project/WiFi.h
+++ Project/WiFi.h
@@ -20,8 +20,12 @@
 //////////////////////////////// VARIABLES ////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 
+template <class DERIVED_TYPE>
 class WiFi
 {
+    friend DERIVED_TYPE;
+    DERIVED_TYPE & derivedType = static_cast <DERIVED_TYPE &> (*this);
+
     public:
         enum class EMode
         {
@@ -36,13 +40,12 @@ class WiFi
             bool SoftApConnected = false;
         };
 
-        static Modes Mode;
+        static inline Modes Mode;
 
-        WiFi          () = default;
-        virtual ~WiFi () = default;
+        WiFi () = default;
 
-        virtual void Reconnect (void) = 0;
         static bool  IsOnline  (void) { return WiFi::Mode.StaConnected; }
+        void         Reconnect (void) { derivedType.Reconnect (); }
 
     protected:
         enum class EEvents
@@ -79,10 +82,48 @@ class WiFi
             } Station = {{ZERO}, {ZERO}}, SoftAp = {{ZERO}, {ZERO}};
         } settings;
 
-        virtual void startStation (void) = 0;
+        void startStation (void) { derivedType.startStation (); }
+        void switchMode (EMode v_eMode)
+        {
+            switch (v_eMode)
+            {
+                case EMode::eStation:
+                {
+                    startStation ();
+                    break;
+                }
+                default:
+                {
+                    break;
+                }
+            }
+        }
 
-        void         switchMode   (EMode v_eMode);
-        static  void onEvent      (EEvents v_event);
+        static void onEvent (EEvents v_event)
+        {
+            if (v_event == EEvents::eStart)
+            {
+                Mode.Started = true;
+                Mode.StaConnected = false;
+            }
+            if (v_event == EEvents::eStop)
+            {
+                Mode.Started = false;
+                Mode.StaConnected = false;
+            }
+            if (v_event == EEvents::eDisconnected || v_event == EEvents::eDisabled || v_event == EEvents::eLostIp)
+            {
+                Mode.StaConnected = false;
+            }
+            else if (v_event == EEvents::eConnected) { }
+            else if (v_event == EEvents::eGotIp)
+            {
+                Mode.StaConnected = true;
+            }
+        }
+
+    private:
+        ~WiFi () = default;
 };
 
 ///////////////////////////////////////////////////////////////////////////////
